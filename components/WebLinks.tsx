ece29c676e55121282714b231a3e018459e03588
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2023-01-05 19:08:27 -0500

    1/5

---
+++ components/WebLinks.tsx
@@ -0,0 +1,190 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const WebLinks = () => {
+  return (
+    <>
+      <Link href="https://www.youtube.com/@Blippi/videos">
+        <div className="h-28 w-28">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/blippi.jpg"
+            alt="blippi"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@babyteeth4/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/jillianandaddie.jpg"
+            alt="jillianandaddie"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@madhorror/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/madhorror.jpg"
+            alt="madhorror"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@CoComelon/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/cocomelon.jpg"
+            alt="cocomelon"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+
+      <Link href="https://www.youtube.com/@msrachel/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/rachel.jpg"
+            alt="rachel"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@TheMikMaks/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/mikmak.jpg"
+            alt="mikmak"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@dbillions/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/dbillions.jpg"
+            alt="dbillions"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+
+      <Link href="https://www.youtube.com/@StuntsAmazing/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/parkour.jpg"
+            alt="parkour"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@armandohibo1/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/funnycat.jpg"
+            alt="funnycat"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://pbskids.org">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/pbs.jpg"
+            alt="pbs"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@kiddosworldtv/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/kiddos.jpg"
+            alt="kiddos"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+
+      <Link href="https://www.youtube.com/playlist?list=PLJYf0JdTApCrVnJpQgUb4BQm_5I3KuvZZ">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/scoobydoo.jpg"
+            alt="scoobydoo"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://banned.video">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/jones.jpg"
+            alt="jones"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/playlist?list=PL_c_E65G1OS6v-ppxy1voHV1LB-owLUqa">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/tomandjerry.jpg"
+            alt="tomandjerry"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@JasonVlogslife/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/jasonvlogs.jpg"
+            alt="jasonvlogs"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+      <Link href="https://www.youtube.com/@ZoeyDoodleDog/videos">
+        <div className="h-28 w-28 ml-8">
+          <Image
+            className="transition-transform hover:scale-95 rounded-lg"
+            src="/images/zoeydoodle.jpg"
+            alt="zoeydoodle"
+            height={250}
+            width={250}
+          />
+        </div>
+      </Link>
+    </>
+  );
+};
+
+export default WebLinks;
